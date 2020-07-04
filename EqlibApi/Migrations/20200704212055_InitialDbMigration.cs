dbb0c36bedfd62667453403ed373514f6bc61e0c
Dytrich NguyeN <nguyen.dytrich@gmail.com>
2020-07-04 14:26:04 -0700

    Add migrations, re-organize directory structure

---
+++ EqlibApi/Migrations/20200704212055_InitialDbMigration.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace EqlibApi.Migrations
+{
+    public partial class InitialDbMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Checkouts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CheckoutDate = table.Column<DateTime>(nullable: false),
+                    DueDate = table.Column<DateTime>(nullable: false),
+                    ReturnDate = table.Column<DateTime>(nullable: false),
+                    CheckoutStatus = table.Column<int>(nullable: false),
+                    ApprovalStatus = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Checkouts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ItemGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Moniker = table.Column<string>(nullable: true),
+                    Make = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true),
+                    Department = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SerialNumber = table.Column<string>(nullable: true),
+                    Availability = table.Column<int>(nullable: false),
+                    OffsiteEligable = table.Column<bool>(nullable: false),
+                    DateAcquired = table.Column<DateTime>(nullable: false),
+                    LastInspected = table.Column<DateTime>(nullable: false),
+                    Condition = table.Column<int>(nullable: false),
+                    Notes = table.Column<string>(nullable: true),
+                    CheckoutId = table.Column<int>(nullable: true),
+                    ItemGroupId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Items_Checkouts_CheckoutId",
+                        column: x => x.CheckoutId,
+                        principalTable: "Checkouts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Items_ItemGroups_ItemGroupId",
+                        column: x => x.ItemGroupId,
+                        principalTable: "ItemGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_CheckoutId",
+                table: "Items",
+                column: "CheckoutId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_ItemGroupId",
+                table: "Items",
+                column: "ItemGroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Checkouts");
+
+            migrationBuilder.DropTable(
+                name: "ItemGroups");
+        }
+    }
+}
