012ef94d02c3d0b2ae9013ec1b12a8f72e1c14de
Dytrich NguyeN <nguyen.dytrich@gmail.com>
2020-08-10 11:33:22 -0700

    Write GET tests

---
+++ EqlibApi.Tests/Unit/Controllers/CheckoutControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using EqlibApi.Models;
+using EqlibApi.Models.Db;
+using EqlibApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace EqlibApi.Tests.Unit.Controllers
+{
+    class CheckoutControllerTest
+    {
+        private CheckoutController controller;
+        private ApplicationContext context;
+        
+        [Test]
+        /// <summary>
+        /// Test for a valid GET request
+        /// </summary>
+        public async Task Get()
+        {
+            var result = await controller.GetCheckouts();
+            Assert.IsInstanceOf<IEnumerable<Checkout>>(result.Value);
+            Assert.AreEqual(context.Checkouts.Count(), result.Value.Count());
+        }
+
+        [Test]
+        /// <summary>
+        /// GET a valid checkout by Id
+        /// </summary>
+        public async Task GetById()
+        {
+            var result = await controller.GetCheckouts(1);
+            Assert.IsInstanceOf<Checkout>(result.Value);
+            Assert.AreEqual(1, result.Value.Id);
+        }
+
+        [Test]
+        /// <summary>
+        /// GET a checkout by Id that does not exist
+        /// </summary>
+        public async Task NotFoundGetId()
+        {
+            var result = await controller.GetCheckouts(2000);
+            Assert.IsInstanceOf<NotFoundResult>(result.Result);
+        }
+    }
+}
---
+++ EqlibApi/Controllers/CheckoutController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EqlibApi.Models;
+using EqlibApi.Models.Db;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EqlibApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CheckoutController : ControllerBase
+    {
+        private readonly ApplicationContext context;
+
+        public CheckoutController(ApplicationContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Checkout>>> GetCheckouts()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Checkout>> GetCheckouts(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
