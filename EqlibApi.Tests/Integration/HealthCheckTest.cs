e736366d0809cdc6a1cb9195947d5cd024256fe7
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2020-08-25 15:35:47 -0700

    Set up integration tests in containerized environment

--- EqlibApi.Tests/Integration/HealthCheckTest.cs
+++ EqlibApi.Tests/Integration/HealthCheckTest.cs
@@ -1,8 +1,11 @@
 ﻿using Eqlib;
+using FluentAssertions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Hosting;
 using NUnit.Framework;
+using System;
+using System.Drawing.Printing;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -12,6 +15,8 @@ namespace EqlibApi.Tests.Integration
     {
         private TestServer _testServer;
         private HttpClient _testClient;
+        private string _apiUrl;
+        private string _endpoint = "/api/v1/health";
 
         [OneTimeSetUp]
         public void Setup()
@@ -22,16 +27,28 @@ namespace EqlibApi.Tests.Integration
 
             _testServer = new TestServer(builder);
             _testClient = _testServer.CreateClient();
+            _apiUrl = Environment.GetEnvironmentVariable("API_URL") + _endpoint;
         }
 
         [Test]
         public async Task HealthController_Pong()
         {
-            var response = await _testClient.GetAsync("/api/v1/health");
+            var response = await _testClient.GetAsync(_endpoint);
             response.EnsureSuccessStatusCode();
             var responseString = await response.Content.ReadAsStringAsync();
 
             Assert.AreEqual("pong", responseString);
+        }
+
+        [Test]
+        public async Task HealthController_PongWithRealClient()
+        {
+            var client = new HttpClient();
+            var result = await client.GetAsync(_apiUrl);
+            result.EnsureSuccessStatusCode();
+            var resultString = await result.Content.ReadAsStringAsync();
+
+            Assert.AreEqual("pong", resultString);
         }
     }
 }
