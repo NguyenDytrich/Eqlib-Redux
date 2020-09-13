0e5bdcc158698e2df0f89fb34d10ca00532a3acb
Dytrich Nguyen <nguyen.dytrich@gmail.com>
2020-09-13 10:56:06 -0700

    Add snapshot testing

---
+++ EqlibApi.Tests/Unit/GraphQL/ItemGroupTypeTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using EqlibApi.GraphQL;
+using EqlibApi.Models;
+using HotChocolate;
+using NUnit.Framework;
+using Snapper;
+using Snapper.Attributes;
+
+namespace EqlibApi.Tests.Unit.GraphQL
+{
+    public class ItemGroupTypeTest
+    {
+        [Test]
+        /// <summary>
+        /// Snapshot test to notify us if the schema changes
+        /// </summary>
+        public void ItemGroup_Snapshot_Matches()
+        {
+            ISchema schema = Schema.Create(c =>
+            {
+                c.RegisterQueryType<ItemGroupType>();
+            });
+
+            var schemaDSL = schema.ToString();
+
+            schemaDSL.ShouldMatchSnapshot();
+        }
+    }
+}
